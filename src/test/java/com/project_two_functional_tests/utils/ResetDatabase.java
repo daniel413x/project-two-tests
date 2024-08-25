16cdde2ffbbdb20ae9eedef9647bacef70c8a7e4
Daniel Maramba <daniel413x@gmail.com>
2024-08-25 03:44:09 -0400

    disable ResetDatabase

--- src/test/java/com/project_two_functional_tests/utils/ResetDatabase.java
+++ src/test/java/com/project_two_functional_tests/utils/ResetDatabase.java
@@ -1,14 +1,14 @@
 package com.project_two_functional_tests.utils;
 
-import java.io.IOException;
-
 public class ResetDatabase {
     public static void run() {
-        try {
-            HttpUtil.sendPostRequest("http://localhost:5000/api/testing/reset");
-        } catch (IOException e) {
-            System.out.println(e);
-        }
+        System.out.println("...");
+    //     try {
+    //         // HttpUtil.sendPostRequest("http://localhost:5000/api/testing/reset");
+    //         System.out.println("...");
+    //     } catch (IOException e) {
+    //         System.out.println(e);
+    //     }
     }
     
 }
